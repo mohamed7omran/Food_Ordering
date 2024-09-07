ad8bb8d14fd8c1aa9ab8a59e2facec716071b80c
mohamed7omran <mo1234512345ed@gmail.com>
2024-09-07 23:15:21 +0300

    registration finished

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,11 +1,11 @@
 "use client";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 
 const Header = () => {
   const session = useSession();
   console.log(session);
-
+  const status = session.status;
   return (
     <header className="flex items-center justify-between">
       <nav className="flex items-center gap-8 text-gray-500 font-semibold">
@@ -18,13 +18,25 @@ const Header = () => {
         <Link href={""}>Contact</Link>
       </nav>
       <nav className="flex items-center gap-4 text-gray-500 font-semibold">
-        <Link href={"/login"}>Login</Link>
-        <Link
-          className="bg-primary rounded-full text-white px-8 py-2"
-          href={"/register"}
-        >
-          Register
-        </Link>
+        {status === "authenticated" && (
+          <button
+            onClick={() => signOut({ callbackUrl: "/login" })}
+            className="bg-primary rounded-full text-white px-8 py-2"
+          >
+            Logout
+          </button>
+        )}
+        {status !== "authenticated" && (
+          <>
+            <Link href={"/login"}>Login</Link>
+            <Link
+              className="bg-primary rounded-full text-white px-8 py-2"
+              href={"/register"}
+            >
+              Register
+            </Link>
+          </>
+        )}
       </nav>
     </header>
   );
--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -14,12 +14,12 @@ const Hero = () => {
           delicious joy in life
         </p>
         <div className="flex gap-4 text-sm">
-          <button className="bg-primary uppercase flex items-center gap-2 text-white px-4 py-2 rounded-full text-sm">
+          <button className="bg-primary flex justify-center uppercase flex items-center gap-2 text-white px-4 py-2 rounded-full text-sm">
             Order noe
             <Right />
           </button>
-          <button className="flex gap-2 py-2 text-gray-600 font-semibold">
-            Lern more <Right />
+          <button className="flex gap-2 py-2 items-center border-0 text-gray-600 font-semibold">
+            Learn more <Right />
           </button>
         </div>
       </div>
